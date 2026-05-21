03f98697192826fd08699c69e33fd8440249cae4
brianbcyang27-prog <brian.bcyang27@gmail.com>
2026-05-21 10:16:17 +0800

    created player tank ( no movement)

--- wiiTank v1/main.js
+++ wiiTank v1/main.js
@@ -1,10 +1,27 @@
-const gameStatus = 0;
+let gameStatus = 0;
+
 function gameStart() {
     if (gameStatus == 0) {
-        alert("welcome to the tank game! use the arrow keys to move and space to shoot. good luck!");
-        gameStatus = 1;
+        alert("welcome to the tank game! use the W,S,A,D keys to move and mouse to shoot. good luck!");
+        gameStatus = "running";
         console.log("Game started! gameStatus : ", gameStatus);
         
+        document.getElementById("introTitle").style.display = "none";
+        document.getElementById("gameInfoBg").style.display = "none";
+        document.getElementById("gameStartButton").style.display = "none";
+        document.body.style.backgroundBlendMode = "color";
+        document.body.style.backgroundColor = "white";
+
+        const tank = document.createElement("div");
+
+        tank.style.width = "50px";
+        tank.style.height = "50px";
+        tank.style.background = "green";
+        tank.style.position = "absolute";
+        tank.style.left = "100px";
+        tank.style.top = "100px";
+
+        document.body.appendChild(tank);
         
     }
 }

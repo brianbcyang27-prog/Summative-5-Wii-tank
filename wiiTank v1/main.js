12fce78c9a5a6e66b844142fb3c6cdb66b3a9948
brianbcyang27-prog <brian.bcyang27@gmail.com>
2026-05-21 10:04:22 +0800

    fixed

--- wiiTank v1/main.js
+++ wiiTank v1/main.js
@@ -1,3 +1,10 @@
+const gameStatus = 0;
 function gameStart() {
-    alert("welcome to the tank game! use the arrow keys to move and space to shoot. good luck!");
+    if (gameStatus == 0) {
+        alert("welcome to the tank game! use the arrow keys to move and space to shoot. good luck!");
+        gameStatus = 1;
+        console.log("Game started! gameStatus : ", gameStatus);
+        
+        
+    }
 }
